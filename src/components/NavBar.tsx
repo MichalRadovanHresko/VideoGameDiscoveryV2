2396cfa427f012d2410fa6d02fd9e72be06a67c8
Misko <m.hresko.dev@gmail.com>
2026-06-03 15:45:50 +0200

    Build search input, color mode button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,14 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import SearchInput from "./SearchInput";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px"></Image>
+      <SearchInput />
+      <ColorModeSwitch />
     </HStack>
   );
 };
